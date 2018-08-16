987f399289fb780cfdec167134a661989d7e9fa0
Jason Howell <jasonrobot64@gmail.com>
2018-08-16 00:13:50 -0700

    added custom.el since makefile is borked

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -1,204 +1,188 @@
-;;; package --- init.el
+;;; package --- Summary
+
+;; its not a package, its just all my customs
+
 ;;; Commentary:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; custom-set-variables - Dont edit below here! ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; Code:
 
-;; do this, since I usually run fish
-(setq shell-file-name "/bin/bash")
-
-;;;;;;;;;;;;;;
-;; Packages ;;
-;;;;;;;;;;;;;;
-
-(require 'package)
-
-(add-to-list 'package-archives
-             '("mepla" . "http://melpa.milkbox.net/packages/") t)
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(use-package better-defaults
-  :ensure t)
-
-(use-package diminish)
-
-(use-package helm
-  :ensure t
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x b" . helm-buffers-list)
-         ("M-y" . helm-show-kill-ring)))
-
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
-(use-package anzu
-  :diminish anzu-mode
-  :config (global-anzu-mode))
-
-;; (use-package cider
-;;   :config
-;;   (setq clojure-indent-style :always-indent))
-
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay nil)
-  :bind (("M-SPC" . company-complete)))
-
-(use-package helm-company
-  :bind (("M-S-SPC" . helm-company)))
-
-(use-package projectile
-  :config
-  (unbind-key "C-c p l" projectile-mode-map)
-  (unbind-key "C-c p f" projectile-mode-map))
-
-(use-package helm-projectile
-  :bind (("C-c p s" . helm-projectile-rg)))
-
-(use-package js2-mode
-  :ensure t
-  :mode "\\.js\\'"
-  :bind (("C-." . js2-next-error))
-  :config
-  (setq js2-global-externs
-        '("ontraport" "describe" "it" "beforeEach" "afterEach" "beforeAll" "afterAll" "expect" "jasmine"
-          "test_runner" "steal" "$" "$l" "_")))
-
-;; elpy and tide are disabled, try to replace them with eglot or lsp-mode! Except for JS2. for now.
-;; (use-package elpy
-;;   :ensure t
-;;   :config
-;;   (setq elpy-rpc-python-command "python3")
-;;   :init (elpy-enable))
-
-(use-package web-mode
-  :mode "\\.ejs\\'")
-
-;; (use-package typescript-mode
-;;   :mode "\\.tsx\\'")
-
-;; (use-package tide
-;;   :config
-;;   (defun setup-tide-mode ()
-;;     "Set up tide mode in the current buffer.  Have hooks call this."
-;;     (tide-setup)
-;;     (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;;     (tide-hl-identifier-mode +1))
-;;   ;; (add-hook 'web-mode-hook
-;;   ;;           (lambda ()
-;;   ;;             (when (string-equal "tsx" (file-name-extension buffer-file-name))
-;;   ;;               (setup-tide-mode))))
-;;   (flycheck-add-mode 'typescript-tslint 'web-mode)
-;;   (setq company-tooltip-align-annotations t)
-;;   (add-hook 'before-save-hook 'tide-format-before-save)
-;;   (add-hook 'typescript-mode-hook #'setup-tide-mode))
-
-(use-package slime
-  :config
-  (setq inferior-lisp-program "sbcl"))
-
-;; (use-package flymake
-;;   :bind (("M-n" . flymake-goto-next-error)
-;;          ("M-p" . flymake-goto-prev-error)))
-
-;; keep this package after projectile, so these key bindings are last
-(use-package helm-ls-git
-  :ensure t
-  :bind (("C-c p f" . helm-ls-git-ls)))
-
-;;;;;;;;;;;
-;; Modes ;;
-;;;;;;;;;;;
-;; Any external packages should be in the previous section.
-;; This is just for built-in modes.
-
-(global-hl-line-mode 1)
-(tool-bar-mode -1)
-(global-subword-mode 1)
-(column-number-mode 1)
-
-(add-hook 'prog-mode-hook 'highlight-indentation-mode)
-(add-hook 'prog-mode-hook 'auto-save-mode)
-
-(add-hook 'emacs-lisp-mode-hook 'flymake-mode)
-
-(add-hook 'before-save-hook
-          'delete-trailing-whitespace)
-
-;;;;;;;;;;;;;;;
-;; Functions ;;
-;;;;;;;;;;;;;;;
-
-(defun beginning-of-line-or-indentation ()
-  "Move to beginning of line, or indentation."
-  (interactive)
-  (let ((start (point)))
-    (back-to-indentation)
-    ;; now if point is the same as when we started, we're already at indent start
-    (if (= start (point))
-        (beginning-of-line))))
-
-(defun insert-line-before-and-indent ()
-  "Insert a new line before the current line, keeping both at the same level of indentation."
-  (interactive)
-  (beginning-of-line)
-  (newline)
-  (forward-line -1)
-  (indent-for-tab-command))
-
-(defun copy-file-name ()
-  "Copy the current file's name to the clipboard."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (if (not filename)
-        (message "No file name for current buffer")
-      (kill-new filename))))
-
-;;;;;;;;;;;;;;;;;
-;; Keybindings ;;
-;;;;;;;;;;;;;;;;;
-
-(windmove-default-keybindings)
-
-(global-set-key (kbd "C-a") 'beginning-of-line-or-indentation)
-
-(global-set-key (kbd "C-o") 'insert-line-before-and-indent)
-
-(global-set-key (kbd "M-;") 'comment-line)
-
-(global-set-key (kbd "C-x g") 'magit-status)
-
-(global-set-key
- (kbd "C-v")
- '(lambda ()
-    (interactive)
-    (scroll-up-command 16)))
-
-(global-set-key
- (kbd "M-v")
- '(lambda ()
-    (interactive)
-    (scroll-up-command -16)))
-
-;;;;;;;;;;;;;;;;;;;
-;; Misc settings ;;
-;;;;;;;;;;;;;;;;;;;
-
-;; (setq display-line-numbers 1)
-
-(setq mouse-wheel-progressive-speed nil)
-(setq inhibit-startup-screen t)
-(add-to-list 'default-frame-alist '(font . "Roboto Mono 8"))
-;; (add-to-list 'default-frame-alist '(font . "Noto Sans Mono 8"))
-
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-(provide 'init)
-;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(beacon-color "#F8BBD0")
+ '(c-basic-offset 4)
+ '(c-default-style
+   (quote
+    ((c-mode . "bsd")
+     (c++-mode . "bsd")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu"))))
+ '(column-number-mode t)
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(company-search-regexp-function (quote company-search-flex-regexp))
+ '(company-show-numbers t)
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "83db918b06f0b1df1153f21c0d47250556c7ffb5b5e6906d21749f41737babb7" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "70f5a47eb08fe7a4ccb88e2550d377ce085fedce81cf30c56e3077f95a2909f2" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "bc4b650c41b16b98166b35da94b366c6a9e1e7883bbf4937c897fb7bd05aa619" "bea5fd3610ed135e6ecc35bf8a9c27277d50336455dbdd2969809f7d7c1f7d79" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(delete-trailing-lines nil)
+ '(diary-entry-marker (quote font-lock-variable-name-face))
+ '(display-line-numbers t)
+ '(emms-mode-line-icon-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *note[] = {
+/* width height num_colors chars_per_pixel */
+\"    10   11        2            1\",
+/* colors */
+\". c #1fb3b3\",
+\"# c None s None\",
+/* pixels */
+\"###...####\",
+\"###.#...##\",
+\"###.###...\",
+\"###.#####.\",
+\"###.#####.\",
+\"#...#####.\",
+\"....#####.\",
+\"#..######.\",
+\"#######...\",
+\"######....\",
+\"#######..#\" };")))
+ '(erc-nick "beizhia")
+ '(evil-emacs-state-cursor (quote ("#D50000" hbar)))
+ '(evil-insert-state-cursor (quote ("#D50000" bar)))
+ '(evil-normal-state-cursor (quote ("#F57F17" box)))
+ '(evil-visual-state-cursor (quote ("#66BB6A" box)))
+ '(fci-rule-color "#eee8d5")
+ '(git-commit-summary-max-length 120)
+ '(global-company-mode t)
+ '(global-subword-mode t)
+ '(gnus-logo-colors (quote ("#528d8d" "#c0c0c0")) t)
+ '(gnus-mode-line-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *gnus-pointer[] = {
+/* width height num_colors chars_per_pixel */
+\"    18    13        2            1\",
+/* colors */
+\". c #1fb3b3\",
+\"# c None s None\",
+/* pixels */
+\"##################\",
+\"######..##..######\",
+\"#####........#####\",
+\"#.##.##..##...####\",
+\"#...####.###...##.\",
+\"#..###.######.....\",
+\"#####.########...#\",
+\"###########.######\",
+\"####.###.#..######\",
+\"######..###.######\",
+\"###....####.######\",
+\"###..######.######\",
+\"###########.######\" };")) t)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-indent-guides-auto-enabled nil)
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#fdf6e3" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+   (quote
+    (("#eee8d5" . 0)
+     ("#B4C342" . 20)
+     ("#69CABF" . 30)
+     ("#69B7F0" . 50)
+     ("#DEB542" . 60)
+     ("#F2804F" . 70)
+     ("#F771AC" . 85)
+     ("#eee8d5" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
+ '(hl-fg-colors
+   (quote
+    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
+ '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(org-startup-truncated t)
+ '(package-selected-packages
+   (quote
+    (helm-projectile helm anzu helm-rg package-build shut-up epl git commander f dash s crystal-mode haxe-mode diminish eglot helm-tramp rust-playground toml-mode cargo company-anaconda alect-themes anaconda-mode arjen-grey-theme clojure-mode flycheck-clojure flycheck-rust helm-cider helm-company hy-mode cask cask-mode json-mode web-mode tide xkcd yasnippet molokai-theme ample-theme apropospriate-theme anti-zenburn-theme labburn-theme zenburn-theme elpy magit fish-completion fish-mode company-lsp lsp-go racket-mode web-beautify better-defaults centered-cursor-mode cider dockerfile-mode expand-region ggtags haskell-mode helm-ls-git helm-ls-hg highlight-indentation js2-mode lsp-mode lsp-rust markdown-mode company slime yaml-mode use-package solarized-theme slime-company rust-mode lua-mode flycheck)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(rainbow-identifiers-choose-face-function (quote rainbow-identifiers-cie-l*a*b*-choose-face) t)
+ '(rainbow-identifiers-cie-l*a*b*-color-count 1024 t)
+ '(rainbow-identifiers-cie-l*a*b*-lightness 80 t)
+ '(rainbow-identifiers-cie-l*a*b*-saturation 25 t)
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(tab-width 4)
+ '(tabbar-background-color "#ffffff")
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#c8805d801780")
+     (60 . "#bec073400bc0")
+     (80 . "#b58900")
+     (100 . "#a5008e550000")
+     (120 . "#9d0091000000")
+     (140 . "#950093aa0000")
+     (160 . "#8d0096550000")
+     (180 . "#859900")
+     (200 . "#66aa9baa32aa")
+     (220 . "#57809d004c00")
+     (240 . "#48559e556555")
+     (260 . "#392a9faa7eaa")
+     (280 . "#2aa198")
+     (300 . "#28669833af33")
+     (320 . "#279993ccbacc")
+     (340 . "#26cc8f66c666")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-ff-dotted-directory ((t (:background "gainsboro" :foreground "black"))))
+ '(helm-ls-git-added-copied-face ((t (:foreground "forest green"))))
+ '(helm-ls-git-added-modified-face ((t (:foreground "forest green"))))
+ '(helm-ls-git-deleted-and-staged-face ((t (:foreground "brown"))))
+ '(helm-ls-git-deleted-not-staged-face ((t (:foreground "brown"))))
+ '(helm-ls-git-modified-and-staged-face ((t (:foreground "deep sky blue"))))
+ '(helm-ls-git-modified-not-staged-face ((t (:foreground "deep sky blue"))))
+ '(helm-ls-git-renamed-modified-face ((t (:foreground "dark orange"))))
+ '(helm-ls-git-untracked-face ((t (:foreground "firebrick3")))))
