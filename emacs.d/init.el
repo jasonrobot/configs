804fd31dcba031d4248c54103cb0b1a3de2ccc0d
Jason Howell <jhowell@ontraport.com>
2019-11-19 18:41:02 -0800

    update emacs configs from work

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,10 +20,14 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+(use-package avy
+  :bind (("C-:" . 'avy-goto-char)))
+
 (use-package better-defaults
   :ensure t)
 
-(use-package diminish)
+(use-package delight
+  :ensure t)
 
 (use-package helm
   :ensure t
@@ -32,12 +36,29 @@
          ("C-x b" . helm-buffers-list)
          ("M-y" . helm-show-kill-ring)))
 
+(defun -strip-newlines-from-json (args) ;(output checker buffer)
+  "Remove newlines from the first element of ARGS."
+  (setcar args (replace-regexp-in-string "\n" "" (car args)))
+  args)
+
 (use-package flycheck
   :ensure t
-  :init (global-flycheck-mode))
+  :bind
+  (("M-g n" . 'flycheck-next-error)
+   ("M-g p" . 'flycheck-previous-error))
+  :init
+  (global-flycheck-mode)
+  :config
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
+  (flycheck-add-mode 'javascript-eslint 'js2-mode)
+  (advice-add 'flycheck-parse-eslint
+              :filter-args
+              #'-strip-newlines-from-json))
 
 (use-package anzu
-  :diminish anzu-mode
+  :delight anzu-mode
   :config (global-anzu-mode))
 
 ;; (use-package cider
@@ -46,29 +67,69 @@
 
 (use-package company
   :ensure t
-  :config
-  (setq company-idle-delay nil)
+  :delight company-mode
+  :config (setq company-idle-delay nil)
   :bind (("M-SPC" . company-complete)))
+
+(use-package crystal-mode
+  :bind (("C-c C-f" . crystal-tool-format)))
 
 (use-package helm-company
   :bind (("M-S-SPC" . helm-company)))
 
-(use-package projectile
+(defun my-fix-diff-range (range)
+  "Change the diff RANGE from using three dots (useless) to two (normal)."
+  (when (stringp range)
+    (s-replace "..." ".." range)))
+
+(defun my-always-make-upstream-origin (branch start-point)
+  "None of your shenanigans, Magit! I ALWAYS want BRANCH upstream to be origin!
+
+This is used to override \"magit-branch-maybe-adjust-upstream\", and it just
+always sets branch.NAME.remote to origin. START-POINT is ignored."
+    (magit-call-git "branch" "--set-upstream-to=origin" branch))
+
+(use-package magit
   :config
+  (advice-add 'magit-diff--dwim
+              :filter-return
+              #'my-fix-diff-range)
+  (advice-add 'magit-branch-maybe-adjust-upstream
+              :override
+              #'my-always-make-upstream-origin))
+
+(use-package projectile
+  :delight
+  '(:eval (let ((mode-string " プロジ"))
+            (if (projectile-project-p)
+                (let ((name (projectile-project-name))
+                      (type (symbol-name (projectile-project-type))))
+                  (concat mode-string
+                          (concat "《" name ":" type "》"))
+                  mode-string))))
+  :config
+  ;; These are some projectile functions I dont care about, and I want them for other uses.
   (unbind-key "C-c p l" projectile-mode-map)
   (unbind-key "C-c p f" projectile-mode-map))
 
 (use-package helm-projectile
   :bind (("C-c p s" . helm-projectile-rg)))
 
+(use-package highlight-indentation
+  :delight highlight-indentation-mode)
+
 (use-package js2-mode
   :ensure t
-  :mode "\\.js\\'"
+  :mode "\\.m?js\\'"
+  :delight "JS²"
   :bind (("C-." . js2-next-error))
   :config
   (setq js2-global-externs
-        '("ontraport" "describe" "it" "beforeEach" "afterEach" "beforeAll" "afterAll" "expect" "jasmine"
-          "test_runner" "steal" "$" "$l" "_")))
+        '("ontraport"
+          "setTimeout" "setInterval" "clearTimeout" "clearInterval"
+          "describe" "it" "beforeEach" "afterEach" "beforeAll" "afterAll" "expect" "jasmine"
+          "Globalize"
+          "test_runner" "steal" "$" "$l" "_" "go" "ObjectAnimate")))
 
 ;; elpy and tide are disabled, try to replace them with eglot or lsp-mode! Except for JS2. for now.
 ;; (use-package elpy
@@ -77,8 +138,34 @@
 ;;   (setq elpy-rpc-python-command "python3")
 ;;   :init (elpy-enable))
 
+(use-package php-mode
+  :mode "\\.php\\'"
+  :config
+  (add-hook 'php-mode-hook (lambda () (flycheck-mode -1))))
+
+;; hyper-useful string library
+(use-package s)
+
+(use-package slime
+  :config
+  (setq inferior-lisp-program "sbcl"))
+
+(use-package tide)
+
 (use-package web-mode
-  :mode "\\.ejs\\'")
+  :mode "\\.\\(ejs\\|erb\\|ecr\\|html\\)\\'"
+  :config
+  (add-hook 'web-mode-hook 'emmet-mode))
+
+(use-package which-key
+  :delight which-key-mode)
+
+;; (use-package yasnippet
+;;   :init (yas-global-mode 1)
+;;   :bind (("C-c <tab>" . yas-expand))
+;;   :config
+;;   (define-key yas-minor-mode-map (kbd "<tab>") nil)
+;;   (define-key yas-minor-mode-map (kbd "TAB") nil))
 
 ;; (use-package typescript-mode
 ;;   :mode "\\.tsx\\'")
@@ -98,10 +185,6 @@
 ;;   (setq company-tooltip-align-annotations t)
 ;;   (add-hook 'before-save-hook 'tide-format-before-save)
 ;;   (add-hook 'typescript-mode-hook #'setup-tide-mode))
-
-(use-package slime
-  :config
-  (setq inferior-lisp-program "sbcl"))
 
 ;; (use-package flymake
 ;;   :bind (("M-n" . flymake-goto-next-error)
@@ -133,25 +216,78 @@
   (forward-line -1)
   (indent-for-tab-command))
 
-(defun copy-file-name ()
-  "Copy the current file's name to the clipboard."
+(defun copy-file-path ()
+  "Copy the current file's path to the clipboard."
   (interactive)
   (let ((filename (buffer-file-name)))
     (if (not filename)
         (message "No file name for current buffer")
       (kill-new filename))))
 
+(defun copy-file-name ()
+  "Copy the current file's name to the clipboard."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not filename)
+        (message "No file name for current buffer")
+      (kill-new (file-name-nondirectory filename)))))
+
 (defun insert-branch-name ()
   "When making a commit in magit, insert the branch's name as the first word of the commit."
   (interactive)
-  (let ((initial-pos (point))
-        (begin-pos 150))
-    (goto-char begin-pos)
-    (move-end-of-line 1)
-    (kill-ring-save begin-pos (point))
-    (goto-char initial-pos)
-    (yank)
+  (unless (string= (thing-at-point 'word) "Merge")
+    (save-excursion
+      (forward-line 4)
+      (let ((branch-line (thing-at-point 'line))
+            (branch-name-regexp "On branch \\(\\w*/\\)?\\(\\w+-?[0-9]*\\)"))
+        (string-match branch-name-regexp branch-line)
+        (let ((ticket-name (match-string 2 branch-line)))
+          (when ticket-name
+            (goto-char 0)
+            (insert ticket-name)))))
+    (end-of-line)
     (insert " ")))
+
+
+
+
+
+;; (defun position-cursor-in-commit-buffer ()
+;;   "Move the cursor to the end of any text in the first line of the commit buffer."
+;;   (interactive)
+;;   (move-end-of-line nil))
+
+;; Add a newline as line 2, then move to end of line 1 (assuming starting at pos 0)
+;; (fset 'fix-commit-editmsg
+;;       [?\C-e return ?\C-b ? ])
+
+(defun fix-commit-editmsg ()
+  "Add an empty line as line 2, move cursor in to position for typing."
+  (interactive)
+  (move-end-of-line nil)
+  (newline)
+  (move-end-of-line 0))
+
+(defun wrap-in-$l ()
+  "Wrap a string in $l()."
+  (interactive)
+  (save-excursion
+    (search-forward "'")
+    (backward-char)
+    (insert "$l( ")
+    (forward-sexp)
+    (insert " )")))
+
+;; A macro/function to find the next function definition in a JS file.
+(fset 'js-next-function
+      [?\C-s return ?: ?  ?? ?f ?u ?n ?c ?t ?i ?o ?n ?  ?? ?\( return])
+
+;; would be better to use a regex for this
+;; var let or const, space, word, colon or (equals with maybe spaces around), function or =>
+;; '(var|let|const)\s\w+(:\s*|\s*=\s*)(function|.*=>)
+
+(fset 'js-prev-function
+      [?\C-r return ?: ?  ?? ?f ?u ?n ?c ?t ?i ?o ?n ?  ?? ?\( return])
 
 ;;;;;;;;;;;
 ;; Modes ;;
@@ -159,13 +295,14 @@
 ;; Any external packages should be in the previous section.
 ;; This is just for built-in modes.
 
-(global-hl-line-mode 1)
+(global-hl-line-mode)
 (tool-bar-mode -1)
-(global-subword-mode 1)
-(column-number-mode 1)
+(global-subword-mode)
+(column-number-mode)
+(recentf-mode)
 
 (add-hook 'prog-mode-hook 'highlight-indentation-mode)
-(add-hook 'prog-mode-hook 'auto-save-mode)
+;; (add-hook 'prog-mode-hook 'auto-save-mode)
 
 (add-hook 'emacs-lisp-mode-hook 'flymake-mode)
 
@@ -173,7 +310,16 @@
           'delete-trailing-whitespace)
 
 (add-hook 'git-commit-setup-hook
-          'insert-branch-name)
+          'fix-commit-editmsg)
+
+(let ((disable-line-numbers
+       '(lambda ()
+          (display-line-numbers-mode -1))))
+  (add-hook 'git-commit-setup-hook disable-line-numbers)
+  (add-hook 'magit-mode-hook disable-line-numbers)
+  (add-hook 'term-mode-hook disable-line-numbers))
+
+(delight '((subword-mode "" "subword")))
 
 ;;;;;;;;;;;;;;;;;
 ;; Keybindings ;;
@@ -189,6 +335,8 @@
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
+(global-set-key (kbd "C-x C-g") 'keyboard-quit)
+
 (global-set-key
  (kbd "C-v")
  '(lambda ()
@@ -201,11 +349,15 @@
     (interactive)
     (scroll-up-command -16)))
 
+(eval-after-load 'js2-mode
+  '(progn
+     (define-key js-mode-map (kbd "C-c f n") 'js-next-function)
+     (define-key js-mode-map (kbd "C-c f p") 'js-prev-function)
+     (define-key js-mode-map (kbd "C-c l") 'wrap-in-$l)))
+
 ;;;;;;;;;;;;;;;;;;;
 ;; Misc settings ;;
 ;;;;;;;;;;;;;;;;;;;
-
-;; (setq display-line-numbers 1)
 
 (setq mouse-wheel-progressive-speed nil)
 (setq inhibit-startup-screen t)
@@ -215,6 +367,7 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+(put 'upcase-region 'disabled nil)
+
 (provide 'init)
 ;;; init.el ends here
-
